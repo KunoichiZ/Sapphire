5e9bdd5c69f9034f12244b84f0dce455206ca2c1
Kaoru <KunoichiZ@users.noreply.github.com>
2020-10-30 17:50:39 -0500

    add test command

--- src/commands/Test/test.ts
+++ src/commands/Test/test.ts
@@ -2,8 +2,12 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Command, CommandOptions } from '@sapphire/framework';
 import { Message } from 'discord.js';
 
+ApplyOptions<CommandOptions>({
+    name: 'test',
+    preconditions: ['OwnerOnly'],
+})
 export default class UserCommand extends Command {
-    public async run(message: Message) {
+    public run(message: Message) {
 		return message.channel.send("test message working!");
 	}
 }
