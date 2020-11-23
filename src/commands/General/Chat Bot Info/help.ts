8b2e9dfbda162e738737b67b4d60df37f57267fa
Kaoru <KunoichiZ@users.noreply.github.com>
2020-11-22 21:38:16 -0600

    Add help command

---
+++ src/commands/General/Chat Bot Info/help.ts
@@ -0,0 +1,37 @@
+// Help command from Gitcord (https://github.com/gitcord-project) Copyright 2020 Charalampos Fanoulis, used under the MIT license
+
+import { ApplyOptions } from '@sapphire/decorators';
+import { Args, Command, CommandOptions } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+
+@ApplyOptions<CommandOptions>({
+    aliases: ['commands', 'cmd', 'cmds', 'h'],
+    description: 'Gives you a list of commands',
+	detailedDescription: 'You may also provide a command, which will return info about that command',
+	preconditions: []
+})
+
+export default class TestCommand extends Command {
+    public async run(message: Message, args: Args) {
+		const command = await args.pickResult('string');
+		if (command.success) return this.commandHelp(message, command.value);
+		return message.channel.send(this.mapCommandsToStr());
+	}
+
+	private async commandHelp(message: Message, cmd: string) {
+		const command = this.client.commands.get(cmd);
+		if (typeof command === 'undefined') return message.channel.send("Couldn't find that command!");
+		return message.channel.send(
+			new MessageEmbed()
+				.setColor(0x1100ff)
+				.setTitle(command.name)
+				.setDescription(`${command.description}`)
+				.addField('\u200B', '\u200B')
+				.addField('In detail:', command.detailedDescription)
+		);
+	}
+
+	private mapCommandsToStr() {
+		return this.client.commands.map((val) => `${val.name} → ${val.description}`).join('\n');
+	}
+}
