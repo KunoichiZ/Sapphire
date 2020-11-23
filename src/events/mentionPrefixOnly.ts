9a68896e771d590ef91eb118b9687facd0d6caf3
Kaoru <KunoichiZ@users.noreply.github.com>
2020-11-22 21:38:51 -0600

    add mentionPrefixOnly event

---
+++ src/events/mentionPrefixOnly.ts
@@ -0,0 +1,17 @@
+// mentionPrefixOnly event from Soumil07 (https://github.com/Soumil07/godfather) Copyright 2020 Soumil07, used under the AGPL-3.0 License
+
+import { Event, Events, PieceContext } from '@sapphire/framework';
+import { Message } from 'discord.js';
+
+export default class extends Event<Events.MentionPrefixOnly> {
+
+	public constructor(context: PieceContext) {
+		super(context, { event: Events.MentionPrefixOnly });
+	}
+
+	public async run(message: Message) {
+		const prefix = await this.client.fetchPrefix(message);
+		return message.channel.send(message.guild ? `My prefix in this server is set to: \`${prefix}\`` : "You don't have to use a prefix in Direct Messages.");
+	}
+
+}
