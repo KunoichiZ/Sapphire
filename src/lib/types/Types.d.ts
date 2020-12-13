97ab7e2347c6cd576127aeebcbc6a53da8ef5594
Kaoru <KunoichiZ@users.noreply.github.com>
2020-12-12 22:40:04 -0600

    Create Types.d.ts

---
+++ src/lib/types/Types.d.ts
@@ -0,0 +1,37 @@
+export const enum PreConditions {
+	OwnerOnly = 'OwnerOnly',
+	GuildOnly = 'GuildOnly'
+}
+
+export const enum Permissions {
+	Administrator = 'administrator',
+	ViewAuditLog = 'viewAuditLog',
+	ManageGuild = 'manageGuild',
+	ManageRoles = 'manageRoles',
+	ManageChannels = 'manageChannels',
+	KickMembers = 'kickMembers',
+	BanMembers = 'banMembers',
+	CreateInstantInvite = 'createInstantInvite',
+	ChangeNickname = 'changeNickname',
+	ManageNicknames = 'manageNicknames',
+	ManageEmojis = 'manageEmojis',
+	ManageWebhooks = 'manageWebhooks',
+	ViewChannel = 'viewChannel',
+	SendMessages = 'sendMessages',
+	SendTtsMessages = 'sendTtsMessages',
+	ManageMessages = 'manageMessages',
+	EmbedLinks = 'embedLinks',
+	AttachFiles = 'attachFiles',
+	ReadMessageHistory = 'readMessageHistory',
+	MentionEveryone = 'mentionEveryone',
+	UseExternalEmojis = 'useExternalEmojis',
+	AddReactions = 'addReactions',
+	Connect = 'connect',
+	Speak = 'speak',
+	Stream = 'stream',
+	MuteMembers = 'muteMembers',
+	DeafenMembers = 'deafenMembers',
+	MoveMembers = 'moveMembers',
+	UseVad = 'useVad',
+	PrioritySpeaker = 'prioritySpeaker'
+}
