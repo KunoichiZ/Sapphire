f191ecb216a5e502cf27f9af492372a73f963c73
Kaoru <KunoichiZ@users.noreply.github.com>
2020-12-12 23:07:53 -0600

    Add stats command

---
+++ src/commands/Misc/stats.ts
@@ -0,0 +1,56 @@
+// Stats command from godfather (https://github.com/Soumil07/godfather) Copyright 2020 Soumil07, used under the AGPL-3.0 License
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command, CommandOptions } from '@sapphire/framework';
+import { roundNumber } from '@sapphire/utilities';
+import { format } from '@utils/durationFormat';
+import { Branding } from '@utils/Branding';
+import { Message, MessageEmbed } from 'discord.js';
+import { cpus } from 'os';
+
+@ApplyOptions<CommandOptions>({
+	description: 'View bot statistics'
+})
+export default class extends Command {
+
+	public async run(message: Message) {
+		return message.channel.send(await this.buildEmbed(message));
+	}
+
+	private async buildEmbed(message: Message) {
+		const prefix = await this.client.fetchPrefix(message);
+		const { generalStatistics, serverStatistics } = this;
+		return new MessageEmbed()
+			.setColor(Branding.BrandingColors.Primary)
+			.setAuthor(this.client.user!.username, this.client.user!.displayAvatarURL({ format: 'png' }))
+			.setDescription(`To add ${this.client.user!.username} to your server, use the \`${Array.isArray(prefix) ? prefix[0] : prefix}invite\` command.`)
+			.addField('Connected To', [
+				`**Servers**: ${generalStatistics.guilds}`,
+				`**Users**: ${generalStatistics.members}`,
+				`**Channels**: ${generalStatistics.channels}`
+			].join('\n'), true)
+			.addField('Server Stats', [
+				`**CPU Load**: ${serverStatistics.cpuLoad.map(load => `${load}%`).join(' | ')}`,
+				`**RAM Used**: ${serverStatistics.ramUsed} (Total: ${serverStatistics.ramTotal})`,
+				`**Uptime**: ${format(this.client.uptime ?? 0)}`
+			].join('\n'), true);
+	}
+
+	private get generalStatistics() {
+		return {
+			guilds: this.client.guilds.cache.size.toLocaleString('en-US'),
+			// eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+			members: this.client.guilds.cache.reduce((a, b) => b.memberCount + a, 0).toLocaleString('en-US'),
+			channels: this.client.channels.cache.size.toLocaleString('en-US')
+		};
+	}
+
+	private get serverStatistics() {
+		const usage = process.memoryUsage();
+		return {
+			cpuLoad: cpus().map(({ times }) => roundNumber(((times.user + times.nice + times.sys + times.irq) / times.idle) * 10000) / 100),
+			ramTotal: `${Math.round(100 * (usage.heapTotal / 1048576)) / 100}MB`,
+			ramUsed: `${Math.round(100 * (usage.heapUsed / 1048576)) / 100}MB`
+		};
+	}
+
+}
