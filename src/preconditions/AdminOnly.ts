439e13335737338c297d6dc931a0554aaf18d1c9
Kaoru <KunoichiZ@users.noreply.github.com>
2020-12-16 00:37:35 -0600

    Create AdminOnly precondition

---
+++ src/preconditions/AdminOnly.ts
@@ -0,0 +1,12 @@
+// AdminOnly precondition is from godfather (https://github.com/Soumil07/godfather) Copyright 2020 Soumil07, used under the AGPL-3.0 License
+import { Precondition } from '@sapphire/framework';
+import { Message, Permissions } from 'discord.js';
+
+export default class extends Precondition {
+
+	public run(message: Message) {
+		if (message.guild && message.member!.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return this.ok();
+		return this.error(this.name, 'This command can only be used by server admins.');
+	}
+
+}
