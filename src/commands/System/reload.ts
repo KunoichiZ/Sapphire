8db0ef6e1b9b475a056e9ad8404ae6f0901c389c
Kaoru <KunoichiZ@users.noreply.github.com>
2020-12-22 23:31:34 -0600

    Add reload command

---
+++ src/commands/System/reload.ts
@@ -0,0 +1,21 @@
+// Reload command is from godfather (https://github.com/Soumil07/godfather) Copyright 2020 Soumil07, used under the AGPL-3.0 License
+import { Args, CommandOptions } from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
+import SapphireCommand from '@lib/SapphireCommand';
+import { Message } from 'discord.js';
+
+@ApplyOptions<CommandOptions>({
+	aliases: ['r'],
+	preconditions: ['OwnerOnly']
+})
+export default class extends SapphireCommand {
+
+	public async run(message: Message, args: Args) {
+		const piece = await args.pickResult('piece');
+		if (!piece.success) throw 'Missing required argument: piece';
+
+		await piece.value.reload();
+		return message.channel.send(`✅ Reloaded ${piece.value.store.name.slice(0, -1)}: ${piece.value.name}`);
+	}
+
+}
