c6a17e035aa77408ff24e3a8af5462795e8f2df1
Kaoru <KunoichiZ@users.noreply.github.com>
2020-12-25 14:31:16 -0600

    Update Branding.ts

--- src/lib/util/Branding.ts
+++ src/lib/util/Branding.ts
@@ -1,18 +1,24 @@
-export const Name = 'Sapphire Birch';
-export const Title = 'Conqueror';
-export const Birthday = 'September 20';
-export const Age = '15';
-export const BloodType = 'O';
-export const Hometown = 'Littleroot';
-export const EyeColor = 'Blue';
-export const HairColor = 'Brown';
+export const enum Character {
+	Name = 'Sapphire Birch',
+	Gender = 'Female',
+	Title = 'Conqueror',
+	Birthday = 'September 20',
+	Age = '15',
+	BloodType = 'O',
+	Hometown = 'Littleroot Town',
+	EyeColor = 'Blue',
+	HairColor = 'Brown',
+	Height = '5\'2" (158 cm)',
+	Weight = '99 lbs (45 kg)',
+	TrainerClass = 'Trainer'
+}
+
 export const Team = ['Rono the Aggron ♂', 'Chic the Blaziken ♀', 'Troppy the Tropius ♂', 'Phado the Donphan ♂', 'Kirly the Gallade ♂'];
 export const AtSea = ['Lorry the Wailord ♂', 'Relly the Relicanth ♂'];
 export const Released = ['Minun ♀'];
-export const Borrowed = ['Electrike ♂', 'Rara ♀', 'Mumu ♂'];
+export const Borrowed = ['Electrike ♂', 'Rara the Gardevoir ♀', 'Mumu the Swampert ♂'];
 export const Befriended = ['Rayquaza ↔ Mega Rayquaza'];
-export const Height = '5\'2" (158 cm)';
-export const Weight = '99 lbs (45 kg)';
+export const Relatives = ['Professor Birch (father), unnamed mother'];
 
 export const enum BrandingColors {
 	Primary = 0x26bbec,
