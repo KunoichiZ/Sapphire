444741cace70de5d33a9edbea9822e96489f1466
Kaoru <KunoichiZ@users.noreply.github.com>
2020-12-25 17:34:29 -0600

    Add server command

---
+++ src/commands/Info/server.ts
@@ -0,0 +1,25 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { CommandOptions } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+import SapphireCommand from '#lib/SapphireCommand';
+import { Timestamp } from '@sapphire/time-utilities';
+import { BrandingColors } from '#utils/Branding';
+
+@ApplyOptions<CommandOptions>({
+	description: 'Shows you information about the server'
+})
+export default class BasicCommand extends SapphireCommand {
+    public async run(message: Message) {
+        const createdAt = new Date(message.guild?.createdTimestamp as number)
+        const timestamp = new Timestamp('MMMM DD, YYYY [at] HH:mm:ss [UTC]Z')
+        const formatted = timestamp.display(createdAt);
+        
+        const serverEmbed = new MessageEmbed()
+        .setColor(BrandingColors.Primary)
+        .addField('Server Name', message.guild?.name, true)
+        .addField('Members', message.guild?.memberCount, true)
+        .addField('Created At', formatted);
+        
+        return message.channel.send(serverEmbed);
+    }
+}
