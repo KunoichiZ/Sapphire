1a4370aa049da44e442698d1fd0512a4ee05dddb
Kaoru <KunoichiZ@users.noreply.github.com>
2020-12-25 15:36:01 -0600

    Add uptime command

---
+++ src/commands/Info/uptime.ts
@@ -0,0 +1,14 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { CommandOptions } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+import SapphireCommand from '#lib/SapphireCommand';
+import { format } from '#utils/durationFormat';
+
+@ApplyOptions<CommandOptions>({
+	description: 'Shows you the bot\'s uptime'
+})
+export default class UptimeCommand extends SapphireCommand {
+    public async run(message: Message) {
+        return message.channel.send(`I've been online for ${format(this.context.client.uptime ?? 0)}.`)
+    }
+}
