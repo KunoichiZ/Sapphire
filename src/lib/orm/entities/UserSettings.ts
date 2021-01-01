c59c2bdb8d06d730b52cc394cb02d8cd946d74f6
Kaoru <KunoichiZ@users.noreply.github.com>
2020-12-31 23:38:45 -0600

    Add friend codes to UserSettings

--- src/lib/orm/entities/UserSettings.ts
+++ src/lib/orm/entities/UserSettings.ts
@@ -6,6 +6,12 @@ export default class User extends BaseEntity {
 	@PrimaryColumn('varchar', { name: 'id', length: 19 })
 	public id!: string;
 
+	@Column('varchar', { name: 'switchfc', default: '', length: 17})
+	public switchfc = '';
+
+	@Column('varchar', { name: 'gofc', default: '', length: 14})
+	public gofc = '';
+
 	@Column('bigint', { name: 'balance', default: 0, transformer: kBigIntTransformer })
 	public balance = 0;
 
@@ -14,17 +20,23 @@ export default class User extends BaseEntity {
 
 	private _balance: number | null;
 	private _vault: number | null;
+	private _switchfc: string | null;
+	private _gofc: string | null;
 
 	public constructor() {
 		super();
 		this._balance = null;
 		this._vault = null;
+		this._switchfc = null;
+		this._gofc = null;
 	}
 
 	@AfterLoad()
 	protected entityLoad() {
 		this._balance = this.balance;
 		this._vault = this.vault;
+		this._switchfc = this.switchfc;
+		this._gofc = this.gofc;
 	}
 
 	@AfterInsert()
@@ -36,11 +48,19 @@ export default class User extends BaseEntity {
 		if (this._vault !== null && this.vault !== this._vault) {
 			this._vault = this.vault;
 		}
+		if (this._switchfc !== null && this.switchfc !== this._switchfc) {
+			this._switchfc = this.switchfc;
+		}
+		if (this._gofc !== null && this.gofc !== this._gofc) {
+			this._gofc = this.gofc;
+		}
 	}
 
 	@AfterRemove()
 	protected entityRemove() {
 		this._balance = null;
 		this._vault = null;
+		this._switchfc = null;
+		this._gofc = null;
 	}
 }
