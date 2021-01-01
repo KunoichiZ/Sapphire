eae612f50633bd4ed1b786b310293925ab651d93
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-01 17:15:11 -0600

    Add changelog command

---
+++ src/commands/Info/changelog.ts
@@ -0,0 +1,20 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { CommandOptions } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+import SapphireCommand from '#lib/SapphireCommand';
+import { stripIndents } from 'common-tags';
+
+@ApplyOptions<CommandOptions>({
+	aliases: ['cl', 'log'],
+	category: 'Info',
+	description: 'Displays the changelog'
+})
+export default class AvatarCommand extends SapphireCommand {
+	public async run(message: Message) {
+		const changelogEmbed = new MessageEmbed();
+		changelogEmbed.setColor('#11806a').setAuthor(`${this.context.client.user?.username}'s changelog`).setDescription(stripIndents`
+        **Rewrite to Sapphire, using the Sapphire Discord.js Bot Framework`);
+
+		return message.channel.send(changelogEmbed);
+	}
+}
