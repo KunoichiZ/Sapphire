bd0bf6657c6988e78b2aae28d547d391dda12569
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-01 16:27:16 -0600

    Update help command

--- src/commands/General/Chat Bot Info/help.ts
+++ src/commands/General/Chat Bot Info/help.ts
@@ -1,38 +1,61 @@
-// Help command from Gitcord (https://github.com/gitcord-project) Copyright 2020 Charalampos Fanoulis, used under the MIT license
-
-import { ApplyOptions } from '@sapphire/decorators';
+// Help command from Dominus (https://github.com/RealShadowNova/dominus) Copyright 2021 RealShadowNova, used under the Apache-2.0 License
 import type { Args, CommandOptions } from '@sapphire/framework';
-import { Message, MessageEmbed } from 'discord.js';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Message, MessageEmbed, APIMessage, TextChannel, NewsChannel } from 'discord.js';
 import SapphireCommand from '#lib/SapphireCommand';
+import { BrandingColors } from '#utils/Branding';
+import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 
 @ApplyOptions<CommandOptions>({
 	aliases: ['commands', 'cmd', 'cmds', 'h'],
 	category: 'Chat Bot Info',
-	description: 'Gives you a list of commands',
+	description: 'Shows you this command!',
 	detailedDescription: 'You may also provide a command, which will return info about that command',
 	preconditions: []
 })
-export default class HelpCommand extends SapphireCommand {
+export default class Help2Command extends SapphireCommand {
 	public async run(message: Message, args: Args) {
-		const command = await args.pickResult('string');
-		if (command.success) return this.commandHelp(message, command.value);
-		return message.channel.send(this.mapCommandsToStr());
+		const commandName = await args.pick('string').catch(() => null);
+
+		if (!commandName) return this.menu(message);
+
+		const command =
+			this.context.client.commands.get(commandName.toLowerCase()) ||
+			this.context.client.commands.find((command) => command.aliases.includes(commandName.toLowerCase()));
+
+		if (!command) throw 'Command not found. To view all commands run `help`';
+
+		const embed = new MessageEmbed().setColor(BrandingColors.Secondary);
+
+		if (command.aliases.length) embed.addField('❯ Aliases', command.aliases.map((alias) => `\`${alias}\``).join(' '));
+
+		embed.addField('❯ Detailed Description', command.detailedDescription || 'No detailed description was provided.').setTimestamp();
+
+		if (command.description) embed.setDescription(command.description);
+
+		return message.channel.send(embed);
 	}
 
-	private async commandHelp(message: Message, cmd: string) {
-		const command = this.context.client.commands.get(cmd);
-		if (typeof command === 'undefined') return message.channel.send("Couldn't find that command!");
-		return message.channel.send(
-			new MessageEmbed()
-				.setColor(0x1100ff)
-				.setTitle(command.name)
-				.setDescription(`${command.description}`)
-				.addField('\u200B', '\u200B')
-				.addField('In detail:', command.detailedDescription)
-		);
-	}
+	private async menu(message: Message) {
+		const categories = new Set<string>();
 
-	private mapCommandsToStr() {
-		return this.context.client.commands.map((val) => `${val.name} → ${val.description}`).join('\n');
+		for (const [, command] of this.context.client.commands.filter((command) => command.category.toLowerCase() !== 'system'))
+			categories.add(command.category);
+
+		return new PaginatedMessage({
+			pages: [...categories.values()].map((category) => (index, pages) =>
+				new APIMessage(message.channel, {
+					embed: new MessageEmbed()
+						.setColor(BrandingColors.Primary)
+						.setTitle(category)
+						.setDescription(
+							this.context.client.commands
+								.filter((command) => command.category.toLowerCase() === category.toLowerCase())
+								.map((command) => `\`${command.name}\` → ${command.description || 'No description was provided.'}`)
+						)
+						.setFooter(`Page ${index + 1} / ${pages.length}`)
+				})
+			)
+		}).run(message.author, message.channel as TextChannel | NewsChannel);
 	}
 }
