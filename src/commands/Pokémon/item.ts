7de83b2c9be8b04e0904818d02c052e5311e5a5d
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-01 21:51:07 -0600

    Add item command

---
+++ src/commands/Pokémon/item.ts
@@ -0,0 +1,43 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { CommandOptions, Args } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+import SapphireCommand from '#lib/SapphireCommand';
+import { fetchGraphQLPokemon, getItemDetailsByFuzzy, parseBulbapediaURL } from '#utils/Pokemon';
+import { toTitleCase } from '@sapphire/utilities';
+import { BrandingColors } from '#utils/Branding';
+
+@ApplyOptions<CommandOptions>({
+	aliases: ['pokeitem', 'bag'],
+	category: 'Pokémon',
+	description: 'Gets data for any given Pokémon item'
+})
+export default class ItemCommand extends SapphireCommand {
+	public async run(message: Message, args: Args) {
+		const item = await args.rest('string');
+		const itemDetails = await this.fetchAPI(item.toLowerCase());
+		const externalResourceData = [
+			`[Bulbapedia](${parseBulbapediaURL(itemDetails.bulbapediaPage)} )`,
+			`[Serebii](${itemDetails.serebiiPage})`,
+			`[Smogon](${itemDetails.smogonPage})`
+		].join(' | ');
+		return message.channel.send(
+			new MessageEmbed()
+				.setColor(BrandingColors.Primary)
+				.setAuthor(`Item - ${toTitleCase(itemDetails.name)}`, 'https://cdn.kunoichiz.me/cdn/images/dex.png')
+				.setThumbnail(itemDetails.sprite)
+				.setDescription(itemDetails.desc)
+				.addField('Generation Introduced', itemDetails.generationIntroduced, true)
+				.addField('Available in Generation 8', itemDetails.isNonstandard === 'Past' ? 'No' : 'Yes', true)
+				.addField('External Resources', externalResourceData)
+		);
+	}
+
+	private async fetchAPI(item: string) {
+		try {
+			const { data } = await fetchGraphQLPokemon<'getItemDetailsByFuzzy'>(getItemDetailsByFuzzy, { item });
+			return data.getItemDetailsByFuzzy;
+		} catch (err) {
+			throw err;
+		}
+	}
+}
