f312b3049e85a5422d452fddce45e81544604b59
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-01 21:17:10 -0600

    Add flavor text command

---
+++ src/commands/Pokémon/flavor.ts
@@ -0,0 +1,42 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { CommandOptions, Args } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+import SapphireCommand from '#lib/SapphireCommand';
+import { fetchGraphQLPokemon, getPokemonFlavorTextsByFuzzy, resolveColor } from '#utils/Pokemon';
+import { toTitleCase } from '@sapphire/utilities';
+import type { DexDetails } from '@favware/graphql-pokemon';
+
+@ApplyOptions<CommandOptions>({
+	aliases: ['flavors', 'dexentries', 'dexentry'],
+	category: 'Pokémon',
+	description: 'Gets ability data for any given Pokémon'
+})
+export default class AbilityCommand extends SapphireCommand {
+	public async run(message: Message, args: Args) {
+		const pokemon = await args.rest('string');
+		const pokemonData = await this.fetchAPI(pokemon.toLowerCase());
+		return message.channel.send(this.buildDisplay(pokemonData));
+	}
+
+	private async fetchAPI(pokemon: string) {
+		try {
+			const { data } = await fetchGraphQLPokemon<'getPokemonDetailsByFuzzy'>(getPokemonFlavorTextsByFuzzy, { pokemon });
+			return data.getPokemonDetailsByFuzzy;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	private buildDisplay(pokemonData: DexDetails) {
+		const display = new MessageEmbed()
+			.setColor(resolveColor(pokemonData.color))
+			.setAuthor(`#${pokemonData.num} - ${toTitleCase(pokemonData.species)}`, 'https://cdn.kunoichiz.me/cdn/images/dex.png')
+			.setThumbnail(pokemonData.sprite);
+
+		for (const flavorText of pokemonData.flavorTexts) {
+			display.addField(`**${flavorText.game}**`, flavorText.flavor);
+		}
+
+		return display;
+	}
+}
