138ddd6c86479b831750c5c209254d1047a6772c
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-02 14:05:44 -0600

    Add credits

--- src/preconditions/OwnerOnly.ts
+++ src/preconditions/OwnerOnly.ts
@@ -1,3 +1,4 @@
+// OwnerOnly precondition is from Skyra (https://github.com/skyra-project/skyra) Copyright 2021 Antonio Román, used under the Apache-2.0 License
 import { Awaited, err, ok, Precondition, Result, UserError } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 import { OWNER_ID } from '#root/config';
