fd0c4ebd058b35be2312e14744bda48433b8a93f
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-02 14:05:47 -0600

    Update GuildOnly.ts

--- src/preconditions/GuildOnly.ts
+++ src/preconditions/GuildOnly.ts
@@ -1,7 +1,7 @@
 import { Awaited, err, ok, Precondition, Result, UserError } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
-export class ClientPrecondition extends Precondition {
+export class GuildPrecondition extends Precondition {
 	public run(message: Message): Awaited<Result<unknown, UserError>> {
 		return message.guild === null ? ok() : err(new UserError('guildOnly', 'This command can only be used in a guild.'));
 	}
