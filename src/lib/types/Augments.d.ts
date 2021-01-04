5741500e985e0985ad741b4a0c75f4b2319157d6
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-04 14:43:11 -0600

    Update Augments.d.ts

--- src/lib/types/Augments.d.ts
+++ src/lib/types/Augments.d.ts
@@ -1,11 +1,12 @@
-import GuildSettingsEntity from '#orm/entities/GuildSettings';
+import { GuildEntity } from '#orm/entities/GuildSettings';
 import { Piece } from '@sapphire/framework';
+import { EmojiResolvable } from 'discord.js';
 
 declare module 'discord.js' {
 	interface Client {
 		readonly version: string;
 		readonly invite: string;
-		settingsCache: Map<string, GuildSettingsEntity>;
+		settingsCache: Map<string, GuildEntity>;
 		fetchGuildPrefix(guild: Guild): Promise<string>;
 	}
 }
@@ -14,6 +15,7 @@ declare module '@sapphire/framework' {
 	interface ArgType {
 		duration: number;
 		piece: Piece;
+		emoji: EmojiResolvable;
 	}
 
 	interface Command {
