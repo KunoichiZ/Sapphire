039630bd3014610929a61f292a823ee814779628
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-05 23:23:48 -0600

    Add WarnEntity

---
+++ src/lib/orm/entities/WarnEntity.ts
@@ -0,0 +1,18 @@
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import GuildEntity from './GuildEntity';
+import UserEntity from './UserEntity';
+
+@Entity('warns')
+export default class WarnEntity extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	public id!: number;
+
+    @Column('varchar', { name: 'reason', length: 1000, nullable: true })
+	public reason: string | null = null;
+
+    @ManyToOne(() => GuildEntity, guild => guild.warns, { eager: true })
+	public guild!: GuildEntity;
+
+	@ManyToOne(() => UserEntity, member => member.warns, { eager: true })
+	public member!: UserEntity;
+}
