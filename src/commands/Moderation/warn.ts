2e5c8e64dba416a0702267a38f73f126e5bc4a40
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-17 21:35:45 -0600

    Update warn.ts

--- src/commands/Moderation/warn.ts
+++ src/commands/Moderation/warn.ts
@@ -1,13 +1,10 @@
+/* eslint-disable no-else-return */
 import { ApplyOptions } from '@sapphire/decorators';
 import type { CommandOptions, Args } from '@sapphire/framework';
 import { Message, MessageEmbed, TextChannel } from 'discord.js';
 import SapphireCommand from '#lib/SapphireCommand';
-import WarnEntity from '#orm/entities/WarnEntity';
-import type UserEntity from '#orm/entities/UserEntity';
-import { getGuild } from '#utils/get';
 import { BrandingColors } from '#utils/Branding';
-import { cast } from '#utils/cast';
-import { CLIENT_ID, OWNER_ID } from '#root/config';
+import { CLIENT_ID, OWNER_ID, POOL } from '#root/config';
 
 @ApplyOptions<CommandOptions>({
 	category: 'Moderation',
@@ -22,34 +19,44 @@ export default class WarnCommand extends SapphireCommand {
 		const member = await message.guild!.members.fetch(user?.id).catch(() => null);
 		if (!member) message.channel.send('**Member not found.** Please make sure the user is in this guild.');
 
-		const guild = await getGuild(message.guild?.id as string);
-
 		const reason = await args.rest('string').catch(() => null);
 
 		if (reason && reason.length > 1000) message.channel.send('Reason maximum char length is 1000.');
 
-		const warn = new WarnEntity();
-		warn.member = cast<UserEntity>(member);
-		warn.id = message.guild?.id as string;
-		warn.caseID = await guild.increaseTotalCases();
-		warn.guild = guild;
-		warn.reason = reason;
-		guild.warns = Object.values(warn);
-		guild.totalWarns = await guild.increaseTotalCases();
-		await warn.save();
-		await guild.save();
-		console.log(warn.caseID);
+		const warnEmbed = new MessageEmbed()
+			.setColor(BrandingColors.Primary)
+			.setAuthor(message.author.tag, message.author.displayAvatarURL())
+			.setDescription([`**Action**: Warn`, `**User**: ${user.tag} (${user.id})`, `**Reason**: ${reason || 'No reason was provided.'}`])
+			.setTimestamp();
 
-		const modLogsChannel = (await getGuild(message.guild?.id as string)).modlogsChannel;
-		const modlogsChannel = message.guild?.channels.cache.find((channel) => channel.name === modLogsChannel) as TextChannel;
+		const guildID = message.guild?.id;
+		const selectWarnQuery = `SELECT id FROM warns WHERE id=${guildID}`;
+		const selectGuildQuery = `SELECT totalwarns, modlogschannel FROM guilds WHERE id=${guildID}`;
 
-		modlogsChannel.send(
-			new MessageEmbed()
-				.setColor(BrandingColors.Primary)
-				.setAuthor(message.author.tag, message.author.displayAvatarURL())
-				.setDescription([`**Action**: Warn`, `**User**: ${user.tag} (${user.id})`, `**Reason**: ${reason || 'No reason was provided.'}`])
-				.setFooter(`Warn Case ${warn.caseID}`, this.context.client.user!.displayAvatarURL())
-				.setTimestamp()
-		);
+		POOL.query(selectGuildQuery, (err, res) => {
+			if (err) {
+				return console.log(err.stack);
+			} else {
+				let { totalwarns } = res.rows[0];
+				const { modlogschannel } = res.rows[0];
+				const modlogsChannel = message.guild?.channels.cache.find((channel) => channel.name === modlogschannel) as TextChannel;
+				totalwarns += 1;
+				console.log(totalwarns);
+				POOL.query(selectWarnQuery, (err, res) => {
+					if (err) {
+						return console.log(err.stack);
+					} else if (res.rows[0] === undefined) {
+						const caseID = totalwarns;
+						POOL.query(`UPDATE guilds SET totalwarns = '${totalwarns}' WHERE id = ${guildID}`);
+						POOL.query(`UPDATE warns SET id = '${user.id}' WHERE id = ${user.id}`);
+						POOL.query(`UPDATE warns SET caseid = '${caseID}' WHERE id = ${user.id}`);
+						POOL.query(`UPDATE warns SET reason = '${reason}' WHERE id = ${user.id}`);
+						POOL.query(`UPDATE warns SET guild = '${guildID}' WHERE id = ${user.id}`);
+						warnEmbed.setFooter(`Warn Case ${caseID}`, this.context.client.user!.displayAvatarURL());
+						modlogsChannel.send(warnEmbed);
+					}
+				});
+			}
+		});
 	}
 }
