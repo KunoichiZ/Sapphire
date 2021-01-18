7538a0a968dbe8011c0dd8f43db100a42d700efd
Kaoru <KunoichiZ@users.noreply.github.com>
2021-01-17 21:34:37 -0600

    Update SapphireClient.ts

--- src/lib/SapphireClient.ts
+++ src/lib/SapphireClient.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-else-return */
 import { SapphireClient } from '@sapphire/framework';
-import type { ClientOptions, Message } from 'discord.js';
-import { POOL, PREFIX } from '#root/config';
+import type { ClientOptions, Guild, Message } from 'discord.js';
+import { PGSQL_ENABLED, POOL, PREFIX } from '#root/config';
 
 export class SBClient extends SapphireClient {
 	private _version = [1, 0, 0];
@@ -11,21 +11,7 @@ export class SBClient extends SapphireClient {
 		this.registerUserDirectories();
 		this.fetchPrefix = (message: Message) => {
 			if (!message.guild) return [PREFIX, ''];
-			const id = message.guild?.id as string;
-			const selectQuery = `SELECT prefix FROM guilds WHERE id=${id}`;
-			let prefix = '';
-			POOL.query(selectQuery, (err, res) => {
-				if (err) {
-					return console.log(err.stack);
-				} else {
-					prefix = res.rows[0].prefix;
-					message.channel.send(
-						message.guild ? `My prefix in this server is set to: \`${prefix}\`` : "You don't have to use a prefix in Direct Messages."
-					);
-					return prefix;
-				}
-			});
-			return PREFIX;
+			return this.fetchGuildPrefix(message.guild);
 		};
 	}
 
@@ -36,5 +22,21 @@ export class SBClient extends SapphireClient {
 	public get version() {
 		const versionStr = this._version.join('.');
 		return versionStr;
+	}
+
+	public async fetchGuildPrefix(guild: Guild) {
+		if (!PGSQL_ENABLED) return PREFIX;
+		const id = guild?.id as string;
+		const selectQuery = `SELECT prefix FROM guilds WHERE id=${id}`;
+		let prefix = '';
+		await POOL.query(selectQuery, (err, res) => {
+			if (err) {
+				return console.log(err.stack);
+			} else {
+				prefix = res.rows[0].prefix;
+				return prefix;
+			}
+		});
+		return PREFIX;
 	}
 }
