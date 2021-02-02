aeb8003cb95fefa85f0ee84157b4fa6f71d398a4
Kaoru <KunoichiZ@users.noreply.github.com>
2021-02-02 12:59:52 -0600

    Update arguments to support @sapphire/framework v1.0.0-alpha.5

--- src/arguments/emoji.ts
+++ src/arguments/emoji.ts
@@ -1,6 +1,6 @@
 // Emoji argument is from Dominus (https://github.com/dominus-project/dominus) Copyright 2021 RealShadowNova, used under the Apache-2.0 License
 import { EmojiRegex, TwemojiRegex } from '@sapphire/discord-utilities';
-import { Argument, PieceContext } from '@sapphire/framework';
+import { Argument, ArgumentResult, PieceContext } from '@sapphire/framework';
 import type { EmojiResolvable } from 'discord.js';
 
 export default class extends Argument<EmojiResolvable> {
@@ -8,12 +8,12 @@ export default class extends Argument<EmojiResolvable> {
 		super(context, { name: 'emoji' });
 	}
 
-	public async run(argument: string) {
-		const emoji = EmojiRegex.exec(argument)?.[3];
-		const twemoji = new RegExp(TwemojiRegex).exec(argument)?.[0] ?? null;
+	public run(parameter: string): ArgumentResult<EmojiResolvable> {
+		const emoji = EmojiRegex.exec(parameter)?.[3];
+		const twemoji = new RegExp(TwemojiRegex).exec(parameter)?.[0] ?? null;
 		const resolvable = emoji ? this.context.client.emojis.cache.get(emoji) : twemoji;
 
-		if (!resolvable) return this.error('EmojiArgument', 'Emoji resolvable not found.');
+		if (!resolvable) return this.error({ parameter, message: 'Emoji resolvable not found.' });
 		return this.ok(resolvable);
 	}
 }
